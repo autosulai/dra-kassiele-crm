66aea3240da9427614230e76ce1b21756f5ae5c8
Cauã Lopes <caua.lopes@mambaculture.com>
2026-07-23 00:14:39 -0300

    fix: adicionar import do useMemo em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Sidebar } from './components/Sidebar';
 import { Funil } from './components/Funil';
 import { Prazos } from './components/Prazos';
